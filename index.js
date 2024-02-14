b168f8c8f5ed39ea7bc1f8fb0be78d8a0a2e859e
mona <mona-rea.nerva@voco.ee>
2024-02-14 11:34:52 +0200

    js

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
 
+// connect to database
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+
+// testing connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connected to the database.')
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database', err)
+  })
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to sequelize application."})
